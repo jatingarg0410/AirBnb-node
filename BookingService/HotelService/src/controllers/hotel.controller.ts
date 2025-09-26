e0eb5aa453260bb5b028fa08a65e9d2d3e399d67
jatingarg0410 <gargjatin0410@gmail.com>
2025-09-27 05:27:02 +0530

    Restructure repo: added BookingService and HotelService as sibling folders

---
+++ BookingService/HotelService/src/controllers/hotel.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response, NextFunction } from 'express'
+import {
+  createHotelService,
+  deleteHotelService,
+  getAllHotelsService,
+  getHotelByIdService,
+  updateHotelService,
+} from '../services/hotel.service'
+import { StatusCodes } from 'http-status-codes'
+
+export async function createHotelHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // 1. Call the service layer
+
+  const hotelResponse = await createHotelService(req.body)
+
+  // 2. Send the response
+
+  res.status(StatusCodes.CREATED).json({
+    message: 'Hotel created successfully',
+    data: hotelResponse,
+    success: true,
+  })
+}
+
+export async function getHotelByIdHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // 1. Call the service layer
+
+  const hotelResponse = await getHotelByIdService(Number(req.params.id))
+
+  // 2. Send the response
+
+  res.status(StatusCodes.OK).json({
+    message: 'Hotel found successfully',
+    data: hotelResponse,
+    success: true,
+  })
+}
+
+export async function getAllHotelsHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // 1. Call the service layer
+
+  const hotelsResponse = await getAllHotelsService()
+
+  // 2. Send the response
+  res.status(StatusCodes.OK).json({
+    message: 'Hotels found successfully',
+    data: hotelsResponse,
+    success: true,
+  })
+}
+
+export async function deleteHotelHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // 1. Call the service layer
+
+  const hotelsResponse = await deleteHotelService(Number(req.params.id))
+
+  // 2. Send the response
+  res.status(StatusCodes.OK).json({
+    message: 'Hotels deleted successfully',
+    data: hotelsResponse,
+    success: true,
+  })
+}
+export async function updateHotelHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // 1. Call the service layer
+
+  const hotelsResponse = await updateHotelService(Number(req.params.id), req.body)
+
+  // 2. Send the response
+  res.status(StatusCodes.OK).json({
+    message: 'Hotels updated successfully',
+    data: hotelsResponse,
+    success: true,
+  })
+}
