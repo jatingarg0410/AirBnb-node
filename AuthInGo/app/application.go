336df48eb232317b02477a4ed2841bba367aba9c
jatingarg0410 <gargjatin0410@gmail.com>
2025-11-28 00:54:25 +0530

    Refactored By Creating Constructors

--- AuthInGo/app/application.go
+++ AuthInGo/app/application.go
@@ -15,6 +15,20 @@ type Application struct{
 Config Config
 }
 
+//Constructor for Application
+func NewApplication(config Config) *Application{
+	return &Application{
+		Config: config,
+	}
+}
+
+//Constructor for Config
+func NewConfig(addr string) Config{
+	return Config{
+		Addr: addr,
+	}
+}
+
 func (app *Application) Run() error{
  server:=&http.Server{
 Addr: app.Config.Addr,
